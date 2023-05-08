cbfaf42a07b8b8f50664223ed49e1cce23522cb9
lovzay <tfvin24@gmail.com>
2023-05-08 10:00:09 +0800

    added preprocessor cucumber, browserify

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify")
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+  return config
+}
 
 module.exports = defineConfig({
   projectId: "196gn7",
@@ -12,14 +21,11 @@ module.exports = defineConfig({
     env: {
       url: "https://rahulshettyacademy.com"
     },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents,
     },
     defaultCommandTimeout: 6000,
-    specPattern: 'cypress/integration/examples/*.js'
-  },
-
-})
+    specPattern: 'cypress/integration/examples/BDD/*.feature'
+  })
 
 
 
